38aa050fdec7aee8fc13cde8c76a33bead651ccb
maelysdarde <maelys@pilot-in.com>
2020-03-16 17:42:05 +0100

    Single meta for options + rename "styles-" to "pip-styles-"

--- functions.php
+++ functions.php
@@ -8,3 +8,11 @@ add_action( 'admin_enqueue_scripts', 'admin_enqueue_pilopress_styles' );
 function admin_enqueue_pilopress_styles() {
     wp_enqueue_style( 'style-pilopress-admin', get_stylesheet_directory_uri() . '/pilopress/style-pilopress-admin.css', false );
 }
+
+add_action( 'acf/init', 'my_acfe_modules' );
+function my_acfe_modules() {
+
+    // Enable Single Meta Save
+    acf_update_setting( 'pip/options/single_meta', true );
+
+}
