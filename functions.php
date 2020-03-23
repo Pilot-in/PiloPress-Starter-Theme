50791222b7221fb0f26e44123ac9fe206d4c2a7a
maelysdarde <maelys@pilot-in.com>
2020-03-23 11:12:20 +0100

    Add version with timestamp value for styles enqueue (Pilo'Press repo #18)

--- functions.php
+++ functions.php
@@ -1,18 +1,35 @@
 <?php
+/**
+ * Enqueue front style
+ */
 add_action( 'wp_enqueue_scripts', 'enqueue_pilopress_styles' );
 function enqueue_pilopress_styles() {
-    wp_enqueue_style( 'style-pilopress', get_stylesheet_directory_uri() . '/pilopress/style-pilopress.css', false );
+    wp_enqueue_style(
+        'style-pilopress',
+        get_stylesheet_directory_uri() . '/pilopress/style-pilopress.css',
+        false,
+        time()
+    );
 }
 
+/**
+ * Enqueue admin style
+ */
 add_action( 'admin_enqueue_scripts', 'admin_enqueue_pilopress_styles' );
 function admin_enqueue_pilopress_styles() {
-    wp_enqueue_style( 'style-pilopress-admin', get_stylesheet_directory_uri() . '/pilopress/style-pilopress-admin.css', false );
+    wp_enqueue_style(
+        'style-pilopress-admin',
+        get_stylesheet_directory_uri() . '/pilopress/style-pilopress-admin.css',
+        false,
+        time()
+    );
 }
 
+/**
+ * Enable single meta save for options pages
+ */
 add_action( 'acf/init', 'my_acfe_modules' );
 function my_acfe_modules() {
-
-    // Enable Single Meta Save
     acf_update_setting( 'pip/options/single_meta', true );
-
 }
+
