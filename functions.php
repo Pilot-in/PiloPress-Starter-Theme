600ecb16c5e73f3c3d40d89e7d44eb159af8b5b7
maelysdarde <maelys@pilot-in.com>
2020-04-15 10:48:34 +0200

    add pip_enqueue_style() and pip_enqueue_admin_style() functions + rename "pilopres_*" functions to "pip_*"

--- functions.php
+++ functions.php
@@ -4,11 +4,7 @@
  */
 add_action( 'wp_enqueue_scripts', 'enqueue_pilopress_styles' );
 function enqueue_pilopress_styles() {
-    wp_enqueue_style(
-        'style-pilopress',
-        get_stylesheet_directory_uri() . '/pilopress/tailwind/tailwind.min.css',
-        false
-    );
+    pip_enqueue_style();
 }
 
 /**
@@ -16,11 +12,7 @@ function enqueue_pilopress_styles() {
  */
 add_action( 'admin_enqueue_scripts', 'admin_enqueue_pilopress_styles' );
 function admin_enqueue_pilopress_styles() {
-    wp_enqueue_style(
-        'style-pilopress-admin',
-        get_stylesheet_directory_uri() . '/pilopress/tailwind/tailwind-admin.min.css',
-        false
-    );
+    pip_enqueue_admin_style();
 }
 
 /**
