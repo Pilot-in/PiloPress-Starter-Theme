62ce30241796df350b3dc606fd9e4a3c2a7854ed
maelysdarde <maelys@pilot-in.com>
2020-04-21 17:10:48 +0200

    make hero layout more common

--- header.php
+++ header.php
@@ -12,4 +12,38 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<header>
+    <?php $menu_items = wp_get_nav_menu_items( 'Main menu' ); ?>
+
+    <?php if ( $menu_items ): ?>
+        <nav>
+            <ul class="flex items-center w-full h-16 bg-white shadow">
+
+                <li class="mx-10">
+                    <a href="<?php echo home_url() ?>">
+                        <img class="h-12" src="<?php echo wp_upload_dir( '2020/04' )['url'] ?>/logo-pilot-in.png" alt="">
+                    </a>
+                </li>
+
+                <?php foreach ( $menu_items as $menu_item ) : ?>
+
+                    <?php
+                    $li_class = '';
+                    $a_class  = 'text-gray-500';
+                    if ( acf_get_current_url() === $menu_item->url ) {
+                        $li_class = 'border-b-2 border-blue-500';
+                        $a_class  = 'text-gray-700';
+                    }
+                    ?>
+
+                    <li class="mr-3 h-full flex items-center <?php echo $li_class; ?>">
+                        <a class="py-5 px-4 font-semibold <?php echo $a_class ?>" href="<?php echo $menu_item->url; ?>">
+                            <?php echo $menu_item->title; ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </nav>
+    <?php endif; ?>
+</header>
 <?php wp_body_open(); ?>
--- pilopress/layouts/hero/hero.php
+++ pilopress/layouts/hero/hero.php
@@ -3,11 +3,11 @@ $content = get_sub_field( 'content' );
 $bg_img  = get_sub_field( 'background_image' );
 ?>
 
-<div class="md:flex items-center bg-gray-100">
+<div class="md:flex items-center">
     <div class="p-8 sm:mb-10 md:w-1/2 md:mr-4 xl:pl-40">
         <?php echo $content; ?>
     </div>
-    <div class="md:w-1/2 hidden sm:inline-block">
+    <div class="md:w-1/2 sm:inline-block">
         <img src="<?php echo $bg_img['url'] ?>" alt="<?php echo $bg_img['alt'] ?>">
     </div>
 </div>
