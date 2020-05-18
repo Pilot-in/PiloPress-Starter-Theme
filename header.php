46ac5fb1a990d6cdc859cf1c7721c82e71587f61
maelysdarde <maelys@pilot-in.com>
2020-05-18 12:25:26 +0200

    add header title

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <title><?php echo wp_title( '|', false, 'right' ) . get_bloginfo( 'name' ) ?></title>
+
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
