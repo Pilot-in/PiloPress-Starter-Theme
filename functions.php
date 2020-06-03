9b70737eb2aba0d0141b0d34a2f7ea11eabac7eb
maelysdarde <maelys@pilot-in.com>
2020-06-03 17:34:43 +0200

    Auto add of pip_header, pip_footer and auto-enqueue pip styles + update doc

--- functions.php
+++ functions.php
@@ -3,29 +3,9 @@
 /**
  *  Theme standard - Retro-compatibility > 5.2
  */
-if ( !function_exists('wp_body_open') ) {
+if ( !function_exists( 'wp_body_open' ) ) {
     function wp_body_open() {
         do_action( 'wp_body_open' );
-    }
-}
-
-/**
- * Enqueue front style
- */
-add_action( 'wp_enqueue_scripts', 'enqueue_pilopress_styles' );
-function enqueue_pilopress_styles() {
-    if (function_exists('pip_enqueue')) {
-        pip_enqueue();
-    }
-}
-
-/**
- * Enqueue admin style
- */
-add_action( 'admin_enqueue_scripts', 'admin_enqueue_pilopress_styles' );
-function admin_enqueue_pilopress_styles() {
-    if (function_exists('pip_enqueue_admin')) {
-        pip_enqueue_admin();
     }
 }
 
--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 get_header();
-get_pip_header();
 
 the_pip_content();
 
-get_pip_footer();
 get_footer();
