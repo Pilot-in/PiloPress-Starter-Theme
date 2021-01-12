91cfceaf58af9a740f95a56a386f2c329e184339
maelysdarde <maelys@pilot-in.com>
2021-01-12 15:33:19 +0100

    add common hooks

--- includes/class-project.php
+++ includes/class-project.php
@@ -13,6 +13,11 @@ if ( !class_exists( 'Project' ) ) {
         public function __construct() {
             // WP hooks
             add_action( 'init', array( $this, 'init_hook' ) );
+            add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
+            add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+
+            // Pilo'Press hooks
+            add_filter( 'pip/tailwind/css/after_components', array( $this, 'add_custom_css' ) );
         }
 
         /**
@@ -20,6 +25,23 @@ if ( !class_exists( 'Project' ) ) {
          */
         public function init_hook() {
             // Your code here
+        }
+
+        /**
+         * Enqueue styles and scripts
+         */
+        public function enqueue() {
+            // AlpineJS
+            // wp_enqueue_script( 'alpine-js', '//cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js', array( 'jquery' ), '2.8.0', true );
+        }
+
+        /**
+         * Add custom CSS for Tailwind compilation
+         *
+         * @return string
+         */
+        public function add_custom_css() {
+            return file_get_contents( get_stylesheet_directory() . '/style.css' );
         }
 
     }
