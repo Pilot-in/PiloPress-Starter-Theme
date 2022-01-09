7431b7ed924d0e16a913daa8860ca01660f247b2
damien-pilot-in <damien@pilot-in.com>
2022-01-09 20:41:30 +0100

    Updated postcss.config.js for TailwindCSS v3 (front & admin)

--- pilopress/assets/admin-config/postcss.config.js
+++ pilopress/assets/admin-config/postcss.config.js
@@ -1,9 +1,11 @@
 module.exports = {
     plugins: [
+        require( 'postcss-import' ),
+        require( 'tailwindcss/nesting' )(require('postcss-nesting')),
         require( 'tailwindcss' ),
-        require( 'postcss-nested' ),
-        require( 'autoprefixer' ),
-        require( 'postcss-prefix-selector' )( { prefix: '.-preview' } ),
-        require( 'cssnano' )( { preset: 'default' } )
+        require('postcss-preset-env')({
+            features: { 'nesting-rules': false }
+        }),
+        require( 'postcss-prefix-selector' )( { prefix: '.-preview' } )
     ]
 }
