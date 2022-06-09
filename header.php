ae415b0a019ed2d29fec4e53e9527dc671d2b0c7
Maëlys Darde <62112058+maelys-pilotin@users.noreply.github.com>
2022-06-09 14:24:24 +0200

    Remove useless <title> tag since it's added by wp_head();

--- header.php
+++ header.php
@@ -4,7 +4,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title() ?></title>
 
     <?php wp_head(); ?>
 </head>
